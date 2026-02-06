650b56b8a50d23949813a86311894825d0dd0262
devcauanbernardino <devcauanbernardino@gmail.com>
2026-02-06 18:02:21 -0300

    miniframework remoto

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once "../vendor/autoload.php";
+
+$route = new \App\Route();
+
+echo 'Isso esta funcionando';
+echo '<pre>';
+// print_r($route->getUrl());
+echo '</pre>';
+
+echo '<hr>';
+
+echo '<pre>';
+print_r($route->getRoutes());
+echo '</pre>';
+
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cauan/miniframework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '64784a66efbda51dc96f421fbf95ff968b7d6cbf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cauan/miniframework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '64784a66efbda51dc96f421fbf95ff968b7d6cbf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
