2743995f69037da6f02460f1aa10c8559819d731
devcauanbernardino <devcauanbernardino@gmail.com>
2026-02-15 10:38:13 -0300

    iniciando projeto de estudo

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -6,10 +6,6 @@ namespace App\Controllers;
 use MF\Controller\Action;
 use MF\Model\Container;
 
-//os models
-use App\Models\Produto;
-use App\Models\Info;
-
 
 class IndexController extends Action
 {
